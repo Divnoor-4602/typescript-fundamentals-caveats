dbdab34a0f7a1ee21792a3c489820843a2d0b950
Divnoor <divnoorsingh4602@gmail.com>
2025-09-04 21:02:49 -0700

    Refactor TypeScript examples to use specific types, add optional and readonly properties, and demonstrate intersection and union types

--- index.ts
+++ index.ts
@@ -24,26 +24,25 @@ console.log(typeof tech);
 // TS any type can be used to represent any type, basically having any value and it disables all type checking for that type
 // better to use specific types
 
-let color: any = "blue";
-color = 10; // no error here since any type was used instead of an annotation
+let color: string = "blue";
 console.log(color);
 
 // Function parameters and annotations
-const addOne = (num: number): number => {
-  return num + 1;
-};
+// const addOne = (num: number): number => {
+//   return num + 1;
+// };
 
 // provide default value in the function and return annotations
-const minAge = (age: number = 18): boolean => {
-  if (age >= 18) return true;
+// const minAge = (age: number = 18): boolean => {
+//   if (age >= 18) return true;
 
-  return false;
-};
+//   return false;
+// };
 
 // Void type -> it is to denote the absence of any value, often used with functions
-const printMessage = (message: string): void => {
-  console.log(`This is my ${message}`);
-};
+// const printMessage = (message: string): void => {
+//   console.log(`This is my ${message}`);
+// };
 
 // Never -> somewhat like the any keyword, used to denote that the function will not return anything or that the variable can never have any value
 // when to use the never keyword
@@ -93,3 +92,66 @@ type customType = {
   name: string;
   age: number;
 };
+
+// Optional properties
+// add a question sign to make it optional, ts wont throw an error if you don't provide the optional property
+
+// Readonly
+// if a property in type is readonly, it cannot be reassigned
+
+type User = {
+  name: string;
+  readonly location: string;
+};
+
+const user: User = {
+  name: "div",
+  location: "chandigarh",
+};
+
+console.log(user.location);
+
+// Intersection types
+// Combines multiple types into a single type such that all the methods and properties of both the types are present in the resulting type
+// Properties from both the types MUST be there
+type Person = {
+  name: string;
+  age: number;
+};
+
+type Employee = {
+  id: number;
+  role: string;
+};
+
+type TPersonAndEmployee = Person & Employee;
+
+const rishabh: TPersonAndEmployee = {
+  name: "Rishabh",
+  age: 23,
+  id: 1,
+  role: "CEO",
+};
+
+console.log(rishabh);
+
+// Union of types -> combines multiple types into a single type where the properties of all the types combined may or may not be there
+// All properties DO NOT NEED TO BE THERE
+type Student = {
+  name: string;
+  courseCode: number;
+};
+
+type Professor = {
+  id: number;
+  subject: string;
+};
+
+type TProfessorAndStudent = Student | Professor;
+
+const me: TProfessorAndStudent = {
+  name: "Hello",
+  courseCode: 12,
+};
+
+console.log(me);
