7e17371b35a9d18db930fc9c0c987a6b89f14bef
Divnoor <divnoorsingh4602@gmail.com>
2025-09-04 19:55:02 -0700

    Add TypeScript fundamentals example with type annotations and inference

---
+++ index.ts
@@ -0,0 +1,29 @@
+// TypeScript fundamentals
+
+// Annotations -> allows to catch errors to specify the types of values for the data
+
+// strings
+const myName: string = "Divnoor";
+
+// number
+const myNumber: number = 12;
+
+// boolean
+const myBool: boolean = false;
+
+console.log(`${myName}, ${myNumber}, ${myBool}`);
+
+// Type inference -> Allows the compiler to automatically determine the type of the variable based on its value
+// If you intialise a variable without a type, it will automatically try to infer it.
+
+let tech = "Typescript";
+// tech = 12 -> this gives an error since ts infered the type to be string and it expects that to be consistent
+console.log(typeof tech);
+
+// Any type
+// TS any type can be used to represent any type, basically having any value and it disables all type checking for that type
+// better to use specific types
+
+let color: any = "blue";
+color = 10; // no error here since any type was used instead of an annotation
+console.log(color);
